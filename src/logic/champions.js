0709bfa6485397d9aa51d3eb3b7fbbb896ddac13
Marta Peña <marpeva14@hotmail.com>
2020-06-17 14:18:36 +0200

    top filter

--- src/logic/champions.js
+++ src/logic/champions.js
@@ -4,6 +4,30 @@ function getAllChampions(){
     return axios.get('http://ddragon.leagueoflegends.com/cdn/10.12.1/data/en_US/champion.json')
 }
 
-export {
-    getAllChampions
+const top = ['Aatrox', 'Akali', 'Camille', 'Cassiopeia', "Cho'Gath", 'Darius', 'Dr Mundo', 'Fiora', 'Gangplank', 'Garen', 'Gnar', 'Heimerdinger', 'Illaoi', 'Irelia', 'Jax', 'Jayce', 'Kayle', 'Kennen', 'Kled', 'Malphite', 'Maokai', 'Mordekaiser', 'Nasus', 'Olaf', 'Ornn', 'Pantheon', 'Poppy', 'Quinn', 'Renekton', 'Rengar', 'Riven', 'Rumble', 'Ryze', 'Sett', 'Shen', 'Shyvana', 'Singed', 'Sion', 'Teemo', 'Tryndamere', 'Urgot', 'Vladimir', 'Volibear', 'Wukong', 'Yasuo', 'Yorick']
+const mid = ['Aatrox', 'Ahri', 'Akali', 'Anivia', 'Annie',' Aurelion Sol', 'Azir', 'Brand', 'Camille', 'Cassiopeia', "Cho'Gath", 'Corki', 'Diana', 'Ekko', 'Fizz', 'Galio', 'Heimerdinger', 'Irelia', 'Jayce', 'Karthus', 'Kassadin', 'Katarina', 'LeBlanc', 'Lissandra', 'Lucian', 'Lux', 'Malphite', 'Malzahar', 'Neeko', 'Orianna', 'Ornn', 'Pantheon', 'Qiyana', 'Rumble', 'Ryze', 'Swain', 'Sylas', 'Syndra', 'Taliyah', 'Talon', 'Twisted Fate', 'Veigar', "Vel'Koz", 'Viktor', 'Vladimir', 'Xerath', 'Yasuo', 'Zed', 'Ziggs', 'Zilean', 'Zoe']
+const jungle = ['Amumu', 'Dr Mundo', 'Ekko', 'Elise', 'Evelynn', 'Fiddlesticks', 'Gragas', 'Graves', 'Hecarim', 'Ivern', 'Jarvan IV', 'Jax', 'Karthus', 'Kayn', "Kha'Zix", 'Kindred', 'Lee Sin', 'Maestro Yi', 'Nidalee', 'Nocturne', 'Nunu y Willump', 'Olaf', 'Poppy', 'Rammus', "Rek'Sai", 'Rengar', 'Sejuani', 'Sett', 'Shaco', 'Shyvana', 'Skarner', 'Sylas', 'Trundle', 'Udyr', 'Vi', 'Volibear', 'Warwick', 'Wukong', 'Xin Zhao', 'Zac' ]
+const support = ['Alistar', 'Bardo', 'Blitzcrank', 'Brand', 'Braum', 'Fiddlesticks', 'Galio', 'Janna', 'Karma', 'Leona', 'Lulu', 'Lux', 'Malphite', 'Maokai', 'Morgana', 'Nami', 'Nautilus', 'Pantheon', 'Poppy', 'Pyke', 'Rakan', 'Senna', 'Sett', 'Shaco', 'Shen', 'Sona', 'Soraka', 'Swain', 'Tahm Kench', 'Taric', 'Thresh', "Vel'Koz", 'Xerath', 'Yuumi', 'Zilean', 'Zyra']
+const adc = ['Aphelios', 'Ashe', 'Caitlyn', 'Cassiopeia', 'Draven', 'Ezreal', 'Jhin', 'Jinx', "Kai'Sa", 'Kalista', "Kog'Maw", 'Lucian', 'Miss Fortune', 'Sivir', 'Tristana', 'Twitch', 'Varus', 'Vayne', 'Xayah']
+
+
+async function getTopChampions(){
+    let topChampions = []
+    const response = await axios.get('http://ddragon.leagueoflegends.com/cdn/10.12.1/data/en_US/champion.json')
+    let champions = response.data.data
+    let championsArray = Object.values(champions)
+
+    championsArray.forEach(champ => {
+        if(top.includes(champ.name)){
+            topChampions.push(champ)
+        }
+    })
+    console.log(topChampions)
+    return topChampions
 }
+
+export {
+    getAllChampions,
+    getTopChampions
+}
+
--- src/views/Champions.vue
+++ src/views/Champions.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
         <Header/>
+        <div class="position-nav">
+            <p class="position" @click="retrieveChampions">ALL</p>
+            <p class="position" @click="retrieveTopChampions">TOP</p>
+            <p class="position">JUNGLE</p>
+            <p class="position">MID</p>
+            <p class="position">SUPPORT</p>
+            <p class="position">ADC</p>
+        </div>
         <SingleChampion :champions="champions"/>     
     </div>
 </template>
@@ -8,7 +16,7 @@
 <script>
 import Header from '@/components/Header.vue'
 import SingleChampion from '@/components/SingleChampion.vue'
-import {getAllChampions} from '../logic/champions.js'
+import {getAllChampions, getTopChampions} from '../logic/champions.js'
 
 export default {
     name: 'Champions',
@@ -25,7 +33,12 @@ export default {
         retrieveChampions(){
             getAllChampions().then(response => { 
                 this.champions = response.data.data
-                })
+            })
+        },
+        retrieveTopChampions(){
+            getTopChampions().then(response => {
+                this.champions = response
+            })
             console.log(this.champions)
         }
     },
