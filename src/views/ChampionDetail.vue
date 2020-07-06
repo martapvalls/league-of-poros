581999432ff075ca92455ba1c2bf78f1848e0eed
Marta Peña <marpeva14@hotmail.com>
2020-07-06 13:09:12 +0200

    improve champion detail, adding spells and description

--- src/views/ChampionDetail.vue
+++ src/views/ChampionDetail.vue
@@ -1,6 +1,32 @@
 <template>
-    <div>
-        <h2> {{champion.name}}</h2>
+    <div v-if="champion != null" class="champion-container">
+        <h2 class="champion-name"> {{champion.name}}</h2>
+        <h3 class="champion-title">{{champion.title}}</h3>
+        <div>
+            <img :src="'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/' + champion.name + '_0.jpg'" alt="champion image">
+        </div>
+        <div class="champion-history">
+            <p class="champion-lore">{{champion.lore}}</p>
+            <br>
+            <h2>Spells</h2>
+            <br>
+            <div class="spells">
+                <div  v-for="spell in champion.spells" alt="champion spells" :key="spell.id"  >
+                    <div @click="showSpellDetail(spell)" class="spell-container">
+                        <img :src="'http://ddragon.leagueoflegends.com/cdn/8.24.1/img/spell/' + spell.image.full">
+                        <p> {{spell.name}} </p>
+                    </div>  
+                </div>
+            </div>
+            <br>
+            <div class="spell-detail" v-if="spell">
+                <p> {{spell.description}} </p>
+                <br>
+                <p>Cost: {{spell.costBurn}} </p>
+                <br>
+                <p>Cooldown: {{spell.cooldownBurn}} </p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,17 +37,19 @@ export default {
     name: 'ChampionDetail',
     data(){
         return{
-            champion: null
+            champion: null,
+            spell: null
         }
     },
     methods:{
         retrieveChampion(){
             let champName = this.$route.params.name
-            console.log(champName)
             getSingleChampion(champName).then(response => {
                 this.champion = response.data.data[champName]
-                console.log(this.champion)
             })
+        },
+        showSpellDetail(spell){
+            this.spell = spell
         }
     },
     created(){
@@ -29,3 +57,21 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+
+.champion-container{
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 200px;
+}
+
+.champion-name{width: 100%; font-size: 3rem;}
+.champion-title{width: 100%; font-size: 2rem;}
+.champion-history{width: 65%}
+.champion-lore{padding: 50px 20px 20px 20px;}
+.spells{display: flex; justify-content: space-evenly;}
+.spell-container{min-width: 120px; cursor: pointer;}
+.spell-detail{padding: 0 20px;}
+
+</style>
