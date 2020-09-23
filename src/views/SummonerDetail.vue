29f66bdebdea85b706c71aed88baf4c2a022e63d
Marta Peña <marpeva14@hotmail.com>
2020-09-23 12:17:23 +0200

    back btn summoner detail

--- src/views/SummonerDetail.vue
+++ src/views/SummonerDetail.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="summoner">
        <h1>Summoner Detail</h1>
-       <div class="summoner-info">
+       <p class="summoner-back-btn" @click="onBack">Back</p>
+       <div class="summoner-info" v-if="summoner">
            <img class="summoner-img" :src="`http://ddragon.leagueoflegends.com/cdn/${jsonVersion}/img/profileicon/${summoner.profileIconId}.png`" alt="profile icon">
            <div>
                 <span class="summoner-name"> {{summoner.name}} </span> <br>
@@ -26,12 +27,17 @@ export default {
     },
     computed:{
         ...mapState(['summoner']) 
-    }
-   
+    },
+    methods:{
+        onBack(){
+            this.$router.push({ path: `/summoner` })
+        }
+    } 
 }
 </script>
 
 <style scoped>
+.summoner{padding: 0 200px;}
 .summoner-info{
     border: 3px solid red; 
     display: flex;
@@ -40,5 +46,6 @@ export default {
     }
 .summoner-img{border-radius: 50%; width: 10%; margin: 10px;}
 .summoner-name{font-size: 2rem;}
+.summoner-back-btn{cursor: pointer; text-align: left}
 
 </style>
